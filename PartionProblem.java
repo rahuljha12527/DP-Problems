294fc739cddde8858e5aca90e253277fb5359081
rahuljha661 <rahuljhamansapur@gmail.com>
2020-05-05 00:23:12 +0530

    first commit

---
+++ PartionProblem.java
@@ -0,0 +1,59 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{   
+      public static boolean subSetSum(int n,int arr[],int sum){
+
+     	if(sum==0){
+     		return true;
+     	}
+     	if(n==0 && sum!=0){
+     		return false;
+
+     	}
+
+        if(arr[n-1]>sum){
+
+          return subSetSum(n-1,arr,sum);
+        }
+
+        return subSetSum(n-1,arr,sum-arr[n-1])|| subSetSum(n-1,arr,sum);
+
+     }
+     public static boolean subSetSum(int n,int arr[]){
+
+     	int sum=0;
+
+     	for(int i=0;i<arr.length;i++){
+     		sum=sum+arr[i];
+     	}
+
+         if(sum%2!=0){
+         	return false;
+         } 
+
+         return subSetSum(n,arr,sum/2);
+
+     }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   	Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+
+		if(subSetSum(n,arr)){
+			 System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+	}
+}
